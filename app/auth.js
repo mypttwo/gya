2e529db2538914667a3f19e830e8bd56e33805ac
Random Guy <mypt.two@gmail.com>
2018-02-24 14:49:34 +0530

    Cleanup of auth

--- app/auth.js
+++ app/auth.js
@@ -2,6 +2,7 @@
 
 const secret = require('./config').secret;
 const jwt = require('jsonwebtoken');
+const logger = require('./logger');
 
 let messages = {
     no_token : 'No token provided',
@@ -21,13 +22,19 @@ let generateAuthToken = function(userId){
 let verifyAuthToken = function(req, res, next){
     let authToken = req.headers['x-access-token'];
     if(!authToken){
+        logger.error(`No auth token provided for ${req.method} ${req.path} for user ${req.params.id}`);
         return res.status(403).send({auth : false, message : messages.getFailureMessage(messages.no_token)});
     }
     jwt.verify(authToken, secret, (error, data) => {
         if(error){
+            logger.error(`${error} ${new Error().stack}`);
             return res.status(404).send({auth : false, message : messages.getFailureMessage(messages.verify_failed)});
         }
         req.userId = data.id;
+        if(req.userId != req.params.id){
+            logger.error(`User ${req.userId} is attempting to modify data of user ${req.params.id}`);
+            return res.status(403).send({auth : false, message : messages.getFailureMessage(messages.verify_failed)});
+        }
         next();
     })
 }
